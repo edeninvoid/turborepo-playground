5157b85ce40a1169d8f764d0fc183768d1cd7357
eden.lee <eden.in.void@gmail.com>
2025-06-12 17:25:13 +0900

    feat: first commit with shadcn component

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,58 @@
+'use client'
+
 import { Button } from "@workspace/ui/components/button"
+import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger} from "@workspace/ui/components/dialog";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@workspace/ui/components/select";
+import {useState} from "react";
+import {useTheme} from "next-themes";
 
 export default function Page() {
+  const { theme, setTheme } = useTheme()
+  const [value, setValue] = useState('')
+
+  const selectValueChange = (value: string) => {
+    if (value === 'light') {
+      setValue('light')
+    } else {
+      alert('error')
+      setValue('light')
+    }
+  }
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <div className="flex gap-2">
+          <Button onClick={() => setTheme('light')}>Light</Button>
+          <Button onClick={() => setTheme('dark')}>Dark</Button>
+          <Button onClick={() => setTheme('system')}>System</Button>
+        </div>
+        <Button size="sm" onClick={() => alert('버튼!')}>Button</Button>
+        <Select
+          value={value}
+          onValueChange={selectValueChange}>
+          <SelectTrigger className="w-[180px]" >
+            <SelectValue placeholder="Theme" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="light">Light</SelectItem>
+            <SelectItem value="dark">Dark</SelectItem>
+            <SelectItem value="system">System</SelectItem>
+          </SelectContent>
+        </Select>
+        <Dialog>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your account
+                and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   )
